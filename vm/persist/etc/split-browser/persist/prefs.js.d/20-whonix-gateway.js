b7d9395a5a2a59de6d016b29bac1567e49f046ef
Rusty Bird <rustybird@openmailbox.org>
2016-11-27 15:41:14 +0000

    Disable Socks over Unix domain sockets
    #3

--- vm/persist/etc/split-browser/persist/prefs.js.d/20-whonix-gateway.js
+++ vm/persist/etc/split-browser/persist/prefs.js.d/20-whonix-gateway.js
@@ -1,3 +1,4 @@
+user_pref("extensions.torlauncher.socks_port_use_ipc", false);
 user_pref("extensions.torbutton.settings_method", "custom");
 user_pref("extensions.torbutton.custom.socks_host", "10.137.8.1");
 user_pref("extensions.torbutton.socks_host",        "10.137.8.1");
