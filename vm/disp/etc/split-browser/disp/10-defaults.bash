a5d5922da516d7ad53eaf3bb12ab426dfa9dcd87
Rusty Bird <rustybird@openmailbox.org>
2017-05-17 15:08:29 +0000

    Disable Selfrando and wrapper script for Firefox _helper_
    Selfrando (used since 7.0a4) has a massive 12 seconds startup delay.
    Disable it for the Firefox helper (which is only started to pass a URL
    to the master, then it exits).
    
    start-tor-browser hardcodes the SELFRANDO_write_layout_file environment
    variable, so let's have the helper command start the firefox binary
    directly. This also gets rid of a .1 second delay caused by the wrapper
    script.

--- vm/disp/etc/split-browser/disp/10-defaults.bash
+++ vm/disp/etc/split-browser/disp/10-defaults.bash
@@ -3,4 +3,4 @@ SPLIT_BROWSER_TB_DIR=/var/cache/tb-binary/.tb/tor-browser
 
 split_browser_cd_firefox_profile() { cd "$SPLIT_BROWSER_TB_DIR"/Browser/TorBrowser/Data/Browser/profile.default; }
 split_browser_firefox() { "$SPLIT_BROWSER_TB_DIR"/Browser/start-tor-browser --verbose --allow-remote "$@"; }
-split_browser_firefox_client() { split_browser_firefox "$@"; }
+split_browser_firefox_client() { SELFRANDO_skip_shuffle=1 LD_LIBRARY_PATH=$SPLIT_BROWSER_TB_DIR/Browser/TorBrowser/Tor "$SPLIT_BROWSER_TB_DIR"/Browser/firefox --allow-remote "$@"; }
