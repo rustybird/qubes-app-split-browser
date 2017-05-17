a4ae1e53ebd271d5c79a37a76e8a2dfe20fbcb1c
Rusty Bird <rustybird@openmailbox.org>
2017-05-17 15:08:27 +0000

    Allow different firefox commands for master/helper

--- vm/disp/etc/split-browser/disp/10-defaults.bash
+++ vm/disp/etc/split-browser/disp/10-defaults.bash
@@ -3,3 +3,4 @@ SPLIT_BROWSER_TB_DIR=/var/cache/tb-binary/.tb/tor-browser
 
 split_browser_cd_firefox_profile() { cd "$SPLIT_BROWSER_TB_DIR"/Browser/TorBrowser/Data/Browser/profile.default; }
 split_browser_firefox() { "$SPLIT_BROWSER_TB_DIR"/Browser/start-tor-browser --verbose --allow-remote "$@"; }
+split_browser_firefox_client() { split_browser_firefox "$@"; }
