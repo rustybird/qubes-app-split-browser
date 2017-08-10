aa55557bd8ded8b44a7a7c47b2448f1ef0074741
Rusty Bird <rustybird@net-c.com>
2017-08-10 13:06:53 +0000

    Enable input field in Browser Console
    This also prevents the Web Console copy-and-paste nag, so
    devtools.selfxss.count=5 is redundant.

--- vm/persist/etc/split-browser/persist/prefs.js.d/14-no-nags.js
+++ vm/persist/etc/split-browser/persist/prefs.js.d/14-no-nags.js
@@ -1,5 +1,5 @@
 user_pref("browser.tabs.warnOnClose", false);
-user_pref("devtools.selfxss.count", 5);
+user_pref("devtools.chrome.enabled", true);
 user_pref("extensions.torbutton.launch_warning", false);
 user_pref("extensions.torbutton.show_slider_notification", false);
 user_pref("general.warnOnAboutConfig", false);
