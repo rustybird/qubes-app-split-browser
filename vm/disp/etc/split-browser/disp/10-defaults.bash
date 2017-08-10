94fb660fdfb1fbd3a3e16d65d3a1e56daff0b3d7
Rusty Bird <rustybird@net-c.com>
2017-08-10 13:06:46 +0000

    split_browser_*() -> sb_*()

--- vm/disp/etc/split-browser/disp/10-defaults.bash
+++ vm/disp/etc/split-browser/disp/10-defaults.bash
@@ -2,5 +2,5 @@ SPLIT_BROWSER_CMD_SOCKET=/run/split-browser/cmd
 SPLIT_BROWSER_REQ_SOCKET=/run/split-browser/req
 SPLIT_BROWSER_TB_DIR=/var/cache/tb-binary/.tb/tor-browser
 
-split_browser_cd_firefox_profile() { cd "$SPLIT_BROWSER_TB_DIR"/Browser/TorBrowser/Data/Browser/profile.default; }
-split_browser_firefox() { "$SPLIT_BROWSER_TB_DIR"/Browser/start-tor-browser --verbose --allow-remote "$@"; }
+sb_cd_firefox_profile() { cd "$SPLIT_BROWSER_TB_DIR"/Browser/TorBrowser/Data/Browser/profile.default; }
+sb_firefox() { "$SPLIT_BROWSER_TB_DIR"/Browser/start-tor-browser --verbose --allow-remote "$@"; }
