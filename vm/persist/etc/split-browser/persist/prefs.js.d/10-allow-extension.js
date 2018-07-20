7453f5783e39c9439fbd2df743aa9bb68fb6067c
Rusty Bird <rustybird@net-c.com>
2018-07-20 20:31:32 +0000

    Allow legacy extensions
    This alone still isn't quite enough on newer Firefox versions due to
    additional code signing requirements for legacy extensions.

--- vm/persist/etc/split-browser/persist/prefs.js.d/10-allow-extension.js
+++ vm/persist/etc/split-browser/persist/prefs.js.d/10-allow-extension.js
@@ -1,3 +1,4 @@
 user_pref("xpinstall.signatures.required", false);
+user_pref("extensions.legacy.enabled", true);
 user_pref("extensions.autoDisableScopes", 0);
 user_pref("extensions.showMismatchUI", false);
