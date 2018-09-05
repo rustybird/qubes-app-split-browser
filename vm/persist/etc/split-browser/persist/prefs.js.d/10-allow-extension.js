350d955feb39f38a1dfb04ad0255e309753c6924
Rusty Bird <rustybird@net-c.com>
2018-09-05 17:24:24 +0000

    Weed out some prefs that are the default for Tor Browser 8.0

--- vm/persist/etc/split-browser/persist/prefs.js.d/10-allow-extension.js
+++ vm/persist/etc/split-browser/persist/prefs.js.d/10-allow-extension.js
@@ -1,4 +1,3 @@
 user_pref("xpinstall.signatures.required", false);
 user_pref("extensions.legacy.enabled", true);
-user_pref("extensions.autoDisableScopes", 0);
 user_pref("extensions.showMismatchUI", false);
--- vm/persist/etc/split-browser/persist/prefs.js.d/14-no-nags.js
+++ vm/persist/etc/split-browser/persist/prefs.js.d/14-no-nags.js
@@ -1,4 +1,3 @@
 user_pref("browser.tabs.warnOnClose", false);
-user_pref("devtools.chrome.enabled", true);
 user_pref("extensions.torbutton.launch_warning", false);
 user_pref("general.warnOnAboutConfig", false);
