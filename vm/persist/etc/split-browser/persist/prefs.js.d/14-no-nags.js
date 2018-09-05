5cd1c2d9aa965398c3d4686b1fee28e5873f1258
Rusty Bird <rustybird@net-c.com>
2018-09-05 17:24:22 +0000

    Remove orphaned prefs

--- vm/persist/etc/split-browser/persist/prefs.js.d/14-no-nags.js
+++ vm/persist/etc/split-browser/persist/prefs.js.d/14-no-nags.js
@@ -1,6 +1,4 @@
 user_pref("browser.tabs.warnOnClose", false);
 user_pref("devtools.chrome.enabled", true);
 user_pref("extensions.torbutton.launch_warning", false);
-user_pref("extensions.torbutton.show_slider_notification", false);
 user_pref("general.warnOnAboutConfig", false);
-user_pref("noscript.confirmUnsafeReload", false);
