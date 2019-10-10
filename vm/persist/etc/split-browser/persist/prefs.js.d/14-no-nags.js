0549f6776e359efddeb4d966d932c35e0c6c7ee5
Rusty Bird <rustybird@net-c.com>
2019-10-10 11:00:41 +0000

    Explicitly enable input field in Browser Console again

--- vm/persist/etc/split-browser/persist/prefs.js.d/14-no-nags.js
+++ vm/persist/etc/split-browser/persist/prefs.js.d/14-no-nags.js
@@ -1,3 +1,4 @@
 user_pref("browser.tabs.warnOnClose", false);
+user_pref("devtools.chrome.enabled", true);
 user_pref("extensions.torbutton.launch_warning", false);
 user_pref("general.warnOnAboutConfig", false);
