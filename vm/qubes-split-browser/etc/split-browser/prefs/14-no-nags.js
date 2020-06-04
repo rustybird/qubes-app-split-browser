ff51be1d6716ac4b063e32421e2f885a22c5f2fe
Rusty Bird <rustybird@net-c.com>
2020-06-04 14:07:40 +0000

    14-no-nags.js: Disable "Try Onion Services" doorhanger

--- vm/qubes-split-browser/etc/split-browser/prefs/14-no-nags.js
+++ vm/qubes-split-browser/etc/split-browser/prefs/14-no-nags.js
@@ -3,3 +3,4 @@ pref("browser.tabs.warnOnClose", false);
 pref("devtools.chrome.enabled", true);
 pref("extensions.torbutton.launch_warning", false);
 pref("general.warnOnAboutConfig", false);
+pref("privacy.prioritizeonions.showNotification", false);
