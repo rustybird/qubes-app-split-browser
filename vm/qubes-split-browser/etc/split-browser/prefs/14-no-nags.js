e2d93ee30d695fc9b33cf60169e54f9e38e748dd
Rusty Bird <rustybird@net-c.com>
2020-09-27 10:10:27 +0000

    Remove obsolete (Firefox ESR 68 era) prefs
    14-no-nags.js:
    - general.warnOnAboutConfig became browser.aboutConfig.showWarning,
      which was already included in dfe49d2
    
    32-ui-tweaks.js.EXAMPLE:
    - security.insecure_connection_icon.* are enabled by Firefox upstream
    - spellchecker.dictionary left unset now gives reasonable defaults

--- vm/qubes-split-browser/etc/split-browser/prefs/14-no-nags.js
+++ vm/qubes-split-browser/etc/split-browser/prefs/14-no-nags.js
@@ -2,5 +2,4 @@ pref("browser.aboutConfig.showWarning", false);
 pref("browser.tabs.warnOnClose", false);
 pref("devtools.chrome.enabled", true);
 pref("extensions.torbutton.launch_warning", false);
-pref("general.warnOnAboutConfig", false);
 pref("privacy.prioritizeonions.showNotification", false);
