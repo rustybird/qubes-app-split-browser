ea00313d033e04de0ca35fac9cb5d54077a04b0b
Rusty Bird <rustybird@net-c.com>
2022-03-16 11:34:36 +0000

    Remove obsolete pref

--- vm/qubes-split-browser/etc/split-browser/prefs/15-downloads-directory.js
+++ vm/qubes-split-browser/etc/split-browser/prefs/15-downloads-directory.js
@@ -1,4 +1,3 @@
 pref("browser.download.dir", "/home/user/Downloads");
 pref("browser.download.folderList", 2);
 pref("browser.download.useDownloadDir", true);
-pref("print.print_to_filename", "/home/user/Downloads/mozilla.pdf");
