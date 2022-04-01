c96251226d53e6a5c18d83cd7c77eca6661eb73a
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-01 20:58:57 +0800

    Delete vm directory

--- vm/qubes-split-browser-disp/etc/split-browser-disp/21-tor-browser.bash
+++
@@ -1,7 +0,0 @@
-SB_FIREFOX_DIR=~/.tb/tor-browser/Browser
-SB_FIREFOX=( ./start-tor-browser )
-
-unset TOR_DEFAULT_HOMEPAGE
-unset TOR_SOCKS_IPC_PATH
-export TOR_SOCKS_HOST=10.152.152.10
-export TOR_SKIP_LAUNCH=1
--- vm/qubes-split-browser/etc/split-browser/prefs/14-no-nags.js
+++
@@ -1,5 +0,0 @@
-pref("browser.aboutConfig.showWarning", false);
-pref("browser.tabs.warnOnClose", false);
-pref("devtools.chrome.enabled", true);
-pref("extensions.torbutton.launch_warning", false);
-pref("privacy.prioritizeonions.showNotification", false);
