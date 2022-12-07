4b5fe365de0a35da61592b2071d682f82ec14e25
Rusty Bird <rustybird@net-c.com>
2022-12-07 20:15:52 +0000

    Hide display language notification

--- vm/qubes-split-browser/etc/split-browser/prefs/14-no-nags.js
+++ vm/qubes-split-browser/etc/split-browser/prefs/14-no-nags.js
@@ -2,4 +2,5 @@ pref("browser.aboutConfig.showWarning", false);
 pref("browser.tabs.warnOnClose", false);
 pref("devtools.chrome.enabled", true);
 pref("extensions.torbutton.launch_warning", false);
+pref("intl.language_notification.shown", true);
 pref("privacy.prioritizeonions.showNotification", false);
