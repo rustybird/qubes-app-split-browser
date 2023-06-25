cc5ab5dbed112d543458531cf52a4e55668186fe
Rusty Bird <rustybird@net-c.com>
2023-06-25 14:30:00 +0000

    Update download warning pref for Tor Browser 12.5

--- vm/qubes-split-browser/etc/split-browser/prefs/14-no-nags.js
+++ vm/qubes-split-browser/etc/split-browser/prefs/14-no-nags.js
@@ -1,6 +1,6 @@
 pref("browser.aboutConfig.showWarning", false);
+pref("browser.download.showTorWarning", false);
 pref("browser.tabs.warnOnClose", false);
 pref("devtools.chrome.enabled", true);
-pref("extensions.torbutton.launch_warning", false);
 pref("intl.language_notification.shown", true);
 pref("privacy.prioritizeonions.showNotification", false);
