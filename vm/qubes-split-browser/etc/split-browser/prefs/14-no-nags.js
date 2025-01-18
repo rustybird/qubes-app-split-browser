d5c8e2e58bddffca8833597f2a28e0ba7628a961
Rusty Bird <rustybird@net-c.com>
2025-01-18 11:47:05 +0000

    Hide speech synthesis errors in Reader Mode etc.
    https://support.mozilla.org/en-US/kb/speechd-setup

--- vm/qubes-split-browser/etc/split-browser/prefs/14-no-nags.js
+++ vm/qubes-split-browser/etc/split-browser/prefs/14-no-nags.js
@@ -3,4 +3,5 @@ pref("browser.download.showTorWarning", false);
 pref("browser.tabs.warnOnClose", false);
 pref("devtools.chrome.enabled", true);
 pref("intl.language_notification.shown", true);
+pref("media.webspeech.synth.dont_notify_on_error", true);
 pref("privacy.prioritizeonions.showNotification", false);
